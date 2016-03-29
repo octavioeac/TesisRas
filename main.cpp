241ae84bddcf181cde3eafb40c4fae6fc1baf8d0
Octavio <Octavio@Octavio-VAIO>
2016-03-28 22:36:13 -0600

    dsdsdsd

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Octavio
+ *
+ * Created on 28 de marzo de 2016, 08:41 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"HOLA JULIO PROYECTO REMOTO";
+    return 0;
+}
+
